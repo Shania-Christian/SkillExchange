976f96f2d699ea4eabe4849175fbf97c1f6f487c
Shania <shania003c@gmail.com>
2025-04-17 14:12:33 +0530

    first commit

--- frontend/src/shared/footer.jsx
+++ frontend/src/shared/footer.jsx
@@ -57,7 +57,7 @@ const Footer = () => {
         className="text-center text-dark p-3"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
-        © 2024 Copyright:
+        © 2025 Copyright:
         <a
           className="text-dark ms-2"
           href="/"
